d6d3422a6aaf0f3815f5fd31f72b34c9f0f039aa
Luke Colcott <git@github.com:lukeylunchbox/test.git>
2018-06-14 11:03:24 +1000

    Init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import React from 'react'
+// const react = require('react')
+import ReactDOM from 'react-dom'
+import App from './components/App.js'
+
+const result = App()
+
+ReactDOM.render(result, document.getElementById('root'))
