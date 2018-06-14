30c98e7076d6e99643e19b48a0ac47b94688d79a
Luke Colcott <git@github.com:lukeylunchbox/test.git>
2018-06-14 12:04:03 +1000

    Added style with JSX

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
 import React from 'react'
 
-function App(){
-    return React.createElement('div', {}, 'Hello World')
+//Without JSX
+// import './App.scss'
+
+// function App(){
+//     return React.createElement('div', {}, 
+//     React.createElement('div', {}, "Hello Worlds"),
+//     React.createElement('div', {}, React.createElement('span', {}, " Worlds"))
+// )}
+
+//USING  JSX
+const style = {
+    width: "600px"
+}
+
+function App() {
+    return <div> 
+        <img style= {style} src= 'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=350'/>
+        <h1>HI!</h1>
+        <p>Hello World</p>
+         </div>
 }
 
 export default App
